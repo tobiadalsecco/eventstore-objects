a12a5b09533d480988481dde08db598f621504b8
tobiadalsecco <tobia.dalsecco@gmail.com>
2015-09-30 15:03:30 -0300

    diff

--- cli.js
+++ cli.js
@@ -8,8 +8,8 @@ var configs = {
   },
   debug: false,
   events: {
-    set: ['itemSetName', 'itemSetProp', 'itemOpened'],
-    del: ['itemDelProp'],
+    set: ['set', 'apparelSetName', 'apparelSetProp', 'apparelAddTracing', 'apparelOpened'],
+    del: ['del', 'apparelDelProp'],
     snapshot: 'apparelSnapshot'
   },
   maxEventsBetweenSnapshots: 10,
@@ -26,53 +26,53 @@ eventStoreObjects.onConnect = function(){
 
   switch (process.argv[2]) {
       
-      case 'fetch':
-        var headType = process.argv[4];
-        var headParam = process.argv[5] || null;
-        eventStoreObjects.read(process.argv[3], headType, headParam, false, null, function(err, obj) { // true, {'user':'some'}
-          if (!obj) {
-            console.error('Object not found!');
-          } else {
-            console.info('------ RESULT -----');
-            console.log(util.inspect(obj, false, null, true));
-          }
+    case 'fetch':
+      var headType = process.argv[4];
+      var headParam = process.argv[5] || null;
+      eventStoreObjects.read(process.argv[3], headType, headParam, false, null, function(err, obj) { // true, {'user':'some'}
+        if (!obj) {
+          console.error('Object not found!');
+        } else {
+          console.info('------ RESULT -----');
+          console.log(util.inspect(obj, false, null, true));
+        }
+        eventStoreObjects.disconnect();
+      });
+      break;
+    case 'runTest':
+      var n = 0, id = process.argv[3];
+      setInterval(function(){
+        n++;
+        eventStoreObjects.append(id, 'apparelSetName', {a: n}, function() {});
+      }, 10);
+      setInterval(function(){
+        eventStoreObjects.read(id, 'head', null, false, null, function(err, obj) {});
+      }, 500);
+    default:
+      if(events.set.indexOf(process.argv[2]) > -1){
+        console.log('é um evento SET');
+        var deltas;
+        deltas = JSON.parse(process.argv[4]);
+        eventStoreObjects.append(process.argv[3], process.argv[2], deltas, function() {
+          console.log('ok');
           eventStoreObjects.disconnect();
         });
-        break;
-      case 'runTest':
-        var n = 0, id = process.argv[3];
-        setInterval(function(){
-          n++;
-          eventStoreObjects.append(id, 'apparelSetName', {a: n}, function() {});
-        }, 10);
-        setInterval(function(){
-          eventStoreObjects.read(id, 'head', null, false, null, function(err, obj) {});
-        }, 500);
-      default:
-        if(events.set.indexOf(process.argv[2]) > -1){
-          console.log('é um evento SET');
-          var deltas;
+      } else if(events.del.indexOf(process.argv[2]) > -1){
+        console.log('é um evento DEL');
+        var deltas;
+        try{
           deltas = JSON.parse(process.argv[4]);
-          eventStoreObjects.append(process.argv[3], process.argv[2], deltas, function() {
-            console.log('ok');
-            eventStoreObjects.disconnect();
-          });
-        } else if(events.del.indexOf(process.argv[2]) > -1){
-          console.log('é um evento DEL');
-          var deltas;
-          try{
-            deltas = JSON.parse(process.argv[4]);
-          } catch(e){
-            deltas = process.argv[4];
-          }
-          eventStoreObjects.append(process.argv[3], process.argv[2], deltas, function() {
-            console.log('ok');
-            eventStoreObjects.disconnect();
-          });
-        } else {
-          console.log('NA-DA');
-          eventStoreObjects.disconnect();
+        } catch(e){
+          deltas = process.argv[4];
         }
+        eventStoreObjects.append(process.argv[3], process.argv[2], deltas, function() {
+          console.log('ok');
+          eventStoreObjects.disconnect();
+        });
+      } else {
+        console.log('NA-DA');
+        eventStoreObjects.disconnect();
+      }
   }
 
 };
